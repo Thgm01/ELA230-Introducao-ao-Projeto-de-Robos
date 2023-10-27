52561d647a353158af8f41eca7a8f30fbf039127
Thiago M <rpj134@gmail.com>
2023-10-26 21:33:13 -0300

    botao de home feito

---
+++ software/mainwindow.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+
+from ui_mainwndow import Ui_MainWindow
+from robot import Robot
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.main_win)
+        self.ui.set_hp_button.clicked.connect(self.set_hp_button_clicked)
+
+        self.robot = Robot()
+
+
+    def show(self):
+        self.main_win.show()
+
+    def set_hp_button_clicked(self):
+        print('set_hp_clicado')
+        self.robot.home_position()
+        print(self.robot.atual_angles)
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec_())
--- software/robot.py
+++ software/robot.py
@@ -1,8 +1,8 @@
-from board import SCL, SDA
-import busio
+# from board import SCL, SDA
+# import busio
 
-from adafruit_motor import servo
-from adafruit_pca9685 import PCA9685
+# from adafruit_motor import servo
+# from adafruit_pca9685 import PCA9685
 
 UPPER_LIMIT_PWM = 2000
 LOWER_LIMIT_PWM = 10000
@@ -11,15 +11,14 @@ UPPER_LIMIT_DG = 180
 LOWER_LIMIT_DG = 0
 
 class Robot:
-    def __init__(self, joints_number):
-        self.pca = PCA9685(busio.I2C(SCL, SDA))
-        self.pca.reference_clock_speed = 26624000
-        self.pca.frequency = 60
-        self.joints_number = joints_number
-
-        self.home_angles=list()
-        for i in range(joints_number):
-            self.home_angles.append(90)
+    def __init__(self):
+        # self.pca = PCA9685(busio.I2C(SCL, SDA))
+        # self.pca.reference_clock_speed = 26624000
+        # self.pca.frequency = 60
+        self.joints_number = 4
+        
+        self.atual_angles = [90,90,90,90]
+        self.home_angles = self.atual_angles
 
     def degrees_to_pwm(self, angle_degrees):
         angle_pwm = int((UPPER_LIMIT_PWM - LOWER_LIMIT_PWM)/(UPPER_LIMIT_DG - LOWER_LIMIT_DG)*angle_degrees + LOWER_LIMIT_PWM)
@@ -27,13 +26,14 @@ class Robot:
 
 
     def set_single_joint_angle(self, joint_number, angle_degrees):
-        angle_pwm = self.degrees_to_pwm(angle_degrees)
-        self.pca.channels[joint_number-1].duty_cycle = angle_pwm
+        # angle_pwm = self.degrees_to_pwm(angle_degrees)
+        # self.pca.channels[joint_number-1].duty_cycle = angle_pwm
+        self.atual_angles[joint_number-1] = angle_degrees
 
     def set_all_joints_angle(self, list_angle_degrees):
         for j in range(0, self.joints_number):
-            angle_pwm = self.degrees_to_pwm(list_angle_degrees[j])
-            self.pca.channels[j].duty_cycle = angle_pwm
+            self.set_single_joint_angle(j+1, list_angle_degrees[j])
+
 
     def stop_robot(self):
         self.pca.deinit()
