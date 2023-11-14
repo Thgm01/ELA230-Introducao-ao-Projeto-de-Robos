06e01cebbe218c9880720389012a787a81864dfe
Thiago M <rpj134@gmail.com>
2023-11-14 20:32:45 -0300

    update

--- software/robot.py
+++ software/robot.py
@@ -1,8 +1,8 @@
-# from board import SCL, SDA
-# import busio
+from board import SCL, SDA
+import busio
 
-# from adafruit_motor import servo
-# from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
 
 UPPER_LIMIT_PWM = 2000
 LOWER_LIMIT_PWM = 10000
@@ -12,9 +12,9 @@ LOWER_LIMIT_DG = 0
 
 class Robot:
     def __init__(self):
-        # self.pca = PCA9685(busio.I2C(SCL, SDA))
-        # self.pca.reference_clock_speed = 26624000
-        # self.pca.frequency = 60
+        self.pca = PCA9685(busio.I2C(SCL, SDA))
+        self.pca.reference_clock_speed = 26624000
+        self.pca.frequency = 60
         self.joints_number = 4
         
         self.home_angles = [90,90,90,90]
@@ -27,8 +27,8 @@ class Robot:
 
 
     def set_single_joint_angle(self, joint_number, angle_degrees):
-        # angle_pwm = self.degrees_to_pwm(angle_degrees)
-        # self.pca.channels[joint_number-1].duty_cycle = angle_pwm
+        angle_pwm = self.degrees_to_pwm(angle_degrees)
+        self.pca.channels[joint_number-1].duty_cycle = angle_pwm
         self.atual_angles[joint_number-1] = angle_degrees
         print(f'Set Joint {joint_number}: {angle_degrees}\t | All angles: {self.atual_angles}')
 
--- software/ui_mainwindow.py
+++ software/ui_mainwindow.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(627, 654)
+        MainWindow.resize(627, 625)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.side2_robot = QtWidgets.QLabel(self.centralwidget)
